1524b91ad3fbb835b094a979bfe059eca4745464
akpor26 <onovughe26@gmail.com>
2021-07-14 14:33:05 +0100

    Fragment Commit

---
+++ app/src/main/java/com/onovughe/fragmentactivity/fragments/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.onovughe.fragmentactivity.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.onovughe.fragmentactivity.R
+import com.onovughe.fragmentactivity.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+   private lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       binding = FragmentHomeBinding.inflate((inflater,container, false))
+        return binding.root
+    }
+
+}
